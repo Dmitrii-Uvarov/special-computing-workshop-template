91e6990418b6d3d6b9f725cf01495a00ccce2d51
Dmitrii Uvarov <dmitry.a.uvarov@gmail.com>
2023-11-21 06:47:34 +0300

    feat: add test class and test directory, improve codestyle

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -8,13 +8,26 @@ import java.nio.file.Path;
 public class Task3 {
 
   public static void main(String[] args) {
-    Path strctPath = Path.of(args[1]).resolve("file_structure.txt");
-    String structure = buildTree(Path.of(args[0]));
+    Path r = Path.of(args[0]);
+    Path d = Path.of(args[1]);
+    getFileStructure(r, d);
+  }
+
+  /**
+   * Builds a directory file tree and saves it to a .txt file.
+   *
+   * @param rootdir directory to build file tree for.
+   * @param destdir directory to save the file tree. The tree will be saved in the file
+   *                file_structure.txt.
+   */
+  public static void getFileStructure(Path rootdir, Path destdir) {
+    Path strctPath = destdir.resolve("file_structure.txt");
+    String structure = buildTree(rootdir);
     System.out.println(structure);
     try (var file = new FileWriter(strctPath.toFile())) {
       file.write(structure);
-    } catch (IOException e){
-      throw new RuntimeException("Failed to write directory structure tree to file",e);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to write directory structure tree to file", e);
     }
   }
 
@@ -23,14 +36,15 @@ public class Task3 {
     buildTreeRecursive(root, 0, sb);
     return sb.toString();
   }
+
   public static void buildTreeRecursive(Path root, int i, StringBuilder sb) {
-    printDirectory(root,i,sb);
+    printDirectory(root, i, sb);
     try (var files = Files.list(root)) {
       files.forEach(p -> {
         if (Files.isDirectory(p)) {
-          buildTreeRecursive(p,i+1,sb);
+          buildTreeRecursive(p, i + 1, sb);
         } else {
-          printFile(p,i+1,sb);
+          printFile(p, i + 1, sb);
         }
       });
     } catch (IOException e) {
@@ -40,7 +54,6 @@ public class Task3 {
 
   private static void printFile(Path file, int indent, StringBuilder sb) {
     sb.append(getIndentString(indent));
-    sb.append("+--");
     sb.append(file.getFileName());
     sb.append(System.lineSeparator());
   }
@@ -53,10 +66,14 @@ public class Task3 {
   }
 
   private static String getIndentString(int indent) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < indent; i++) {
-      sb.append("|  ");
+    if (indent == 0) {
+      return "";
     }
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < indent - 1; i++) {
+      sb.append("│   ");
+    }
+    sb.append("├── ");
     return sb.toString();
   }
 }
---
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -0,0 +1,55 @@
+package ru.spbu.apcyb.svp.tasks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+class Task3Test {
+
+  @Test
+  void directoryScanTest() throws IOException {
+    String directoryPath = "src/test/resources/task3/TestDirectory";
+    String treeFolder = "src/test/resources/task3";
+    String pathToFile = "src/test/resources/task3/TestDirectory_expected.txt";
+
+    String g = """ 
+        TestDirectory/\r
+        ├── empty/\r
+        ├── foo/\r
+        │   ├── another_one/\r
+        │   │   ├── another_one/\r
+        │   │   │   ├── another_one/\r
+        │   │   │   │   ├── another_one/\r
+        │   │   │   │   │   ├── another_one/\r
+        │   │   │   │   │   │   ├── DJ_KHALED_WISE_WORDS.txt\r
+        │   ├── bar.txt\r
+        │   ├── baz.txt\r
+        ├── some/\r
+        │   ├── a.txt\r
+        │   ├── b.txt\r
+        │   ├── c.txt\r
+        │   ├── d/\r
+        │   │   ├── E.txt\r
+                """;
+
+    try (FileWriter output = new FileWriter(pathToFile)) {
+      output.write(g);
+    }
+
+    Path path = Path.of(directoryPath);
+    Path filepath = Path.of(treeFolder);
+    Task3.getFileStructure(path, filepath);
+
+    Path expectedPathToResult = filepath.resolve("file_structure.txt");
+    if (Files.notExists(expectedPathToResult)) {
+      fail("Cannot find result file at expected path.");
+    }
+
+    assertEquals(-1, Files.mismatch(Path.of(pathToFile), expectedPathToResult));
+  }
+}
